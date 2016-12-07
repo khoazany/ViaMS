4bf5cea563306075f58263ee104216abe935f4d3
imac <imac@viatick.com>
2016-12-07 19:37:26 +0800

    Add Login page

--- client/components/app/index.js
+++ client/components/app/index.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 export default class App extends Component {
 
   render() {
     return <div>
       <Helmet title='Go + React + Redux = rocks!' />
-      {this.props.children}
+      	<MuiThemeProvider>
+      		{this.props.children}
+      	</MuiThemeProvider>
     </div>;
   }
 
